b2551483b5e57975161df61f3bf6fcad769d707e
murkje <45019052+murkje@users.noreply.github.com>
2019-08-29 13:50:22 +0200

    Improved menu
    floating things are a WIP

---
+++ Community Game Jam/Assets/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Community Game Jam/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
